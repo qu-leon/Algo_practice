b917b5bb5ca8f98417038e9ea952df36fee4ffea
Victoria <victoriakwong@users.noreply.github.com>
2025-03-15 17:08:07 -0700

    initial commit

--- Hard_prac.py
+++ Hard_prac.py
@@ -1,10 +1,11 @@
-""""
+""" "
 This is a practice file for the hard problems in the LeetCode.
 """
+
+
 class Solutions3:
-    def removeInvalidParentheses(self, s: str) -> List[str]:
+    def removeInvalidParentheses(self, s: str):
         """
         Remove the minimum number of invalid parentheses in order to make the input string valid. Return a list of unique strings
         that are valid with the minimum number of invalid parentheses removed.
         """
-        
--- Med_prac.py
+++ Med_prac.py
@@ -1,4 +1,11 @@
-""""
+""" "
 This is a practice file for the medium problems in the LeetCode.
 """
+
+
 class Solutions2:
+    def groupAnagrams(self, strs):
+        """
+        Given an array of strings, group anagrams together.
+        """
+        # uses a hashmap to store the sorted string as the key and the original string as the value
