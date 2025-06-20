ca8d7294fb881881922e1b1105ff684ba5ac0396
Leon Qu <leon.qu@intel.com>
2025-06-20 15:40:28 -0700

    test

--- Test.py
+++ Test.py
@@ -1,13 +1,20 @@
 """
 This is a test file
 """
+
 import unittest
 from Easy_prac import Solutions
 from Med_prac import Solutions2
 from Hard_prac import Solutions3
 
+
 class TestEasy(unittest.TestCase):
+    Solutions
+
 
 class TestMed(unittest.TestCase):
+    Solutions2
+
 
 class TestHard(unittest.TestCase):
+    Solutions3
