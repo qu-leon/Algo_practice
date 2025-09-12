ce80ad636563c90426a9ee3be4be155416e65e83
Leon Qu <leon.qu@intel.com>
2025-09-12 10:27:17 -0700

    test2

--- Test.py
+++ Test.py
@@ -4,12 +4,14 @@ This is a test file
 
 import unittest
 from Easy_prac import Solutions
+from Easy_prac import MyQueue
 from Med_prac import Solutions2
 from Hard_prac import Solutions3
 
 
 class TestEasy(unittest.TestCase):
     Solutions
+    MyQueue
 
 
 class TestMed(unittest.TestCase):
